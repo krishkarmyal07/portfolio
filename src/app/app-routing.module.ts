af8cc64a63098cf8747b79fa9905c8e38be3c614
krishkarmyal07 <krishkarmyal96@gmail.com>
2024-08-26 16:50:16 +0530

    feat: add routing for main module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainModule } from './main/main.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "main",
+    pathMatch: "full",
+  },
+  {
+    path:'main',
+    loadChildren: () => import(`./main/main.module`).then((m)=>m.MainModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
