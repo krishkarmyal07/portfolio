47c39ded162976f18e6ba50a99652cf035297ce7
krishkarmyal07 <krishkarmyal96@gmail.com>
2024-08-26 16:55:04 +0530

    remove  sidebar from app comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { TitlebarComponent } from './titlebar/titlebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SidebarComponent,
-    TitlebarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
