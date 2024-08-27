d6c23643eb4d79a74feab87a1681f93311a3dbc4
krishkarmyal07 <krishkarmyal96@gmail.com>
2024-08-27 21:09:51 +0530

    feat: add temp icon and set route on click

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Router, Event, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,6 +7,7 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
+  route: string = '';
 
   onMouseEnter(event: MouseEvent) {
     const sidebar = event.target as HTMLElement;
@@ -25,11 +27,18 @@ export class SidebarComponent {
       document.getElementById('sidebar-right')?.classList.add('hide-sidebar');
     }
   }
-  constructor() {
-    // setTimeout(function() {
-    //   document.getElementById('sidebar')?.classList.add('mini-sidebar');
-    // }, 3000);
-  }
+  constructor( public router: Router) {
+        setTimeout(() => {
+          this.route = router.url
+        }, 0);
+        console.log(this.route)
+        this.router.events.subscribe((event: Event) => {
+          if(event instanceof NavigationEnd){
+            this.route = event.url
+          }
+        })
+     }
+  
 
   closeSidebar() {
     if (document.getElementById('sidebar')?.classList.contains('mini-sidebar')) {
@@ -41,6 +50,21 @@ export class SidebarComponent {
       document.getElementById('sidebar')?.classList.add('mini-sidebar');
       document.querySelector('body')?.classList.add('sidebar-open')
       // document.getElementById('sidebar-right')?.classList.add('hide-sidebar');
+    }
+  }
+
+  toggleMenu(url: string){
+
+    if(this.route !== url ){
+      return
+    }
+    else if(this.route === url && document.getElementById('sidebar')?.classList.contains('mini-sidebar')){
+      document.getElementById('sidebar')?.classList.remove('mini-sidebar');
+      document.querySelector('body')?.classList.remove('sidebar-open')
+    }
+    else{
+      document.getElementById('sidebar')?.classList.add('mini-sidebar');
+      document.querySelector('body')?.classList.add('sidebar-open')
     }
   }
 }
