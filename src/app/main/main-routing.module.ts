8cde9c7ed25bc3d100917732241bb46d1695801b
krishkarmyal07 <krishkarmyal96@gmail.com>
2024-08-28 22:02:54 +0530

    feat: add new component

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -3,6 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ProjectComponent } from './project/project.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ContactComponent } from './contact/contact.component';
+import { DefaultComponent } from './default/default.component';
+import { ReadmeComponent } from './readme/readme.component';
 
 const routes: Routes = [
   {
@@ -21,7 +27,31 @@ const routes: Routes = [
       {
         path:"about",
         component:AboutComponent
-      }
+      },
+      {
+        path:"welcome",
+        component:WelcomeComponent
+      },
+      {
+        path:"project",
+        component:ProjectComponent
+      },
+      {
+        path:"experience",
+        component: ExperienceComponent
+      },
+      {
+        path:"contact",
+        component:ContactComponent
+      },
+      {
+        path:"default",
+        component:DefaultComponent
+      },
+      {
+        path:"readme",
+        component:ReadmeComponent
+      },
     ]
   }
 ];
