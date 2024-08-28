ad628666980b21c7f8d3b1719784cdff6bf916ee
krishkarmyal07 <krishkarmyal96@gmail.com>
2024-08-28 22:14:52 +0530

    logic to check current route

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router, Event } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
-
+  url: string = ''
+  constructor(public router: Router){
+ 
+    this.router.events.subscribe((event: Event) => {
+      if(event instanceof NavigationEnd){
+        this.url = event.url
+      }
+    })
+  }
 }
