d33fbabbb93c05c965c8a07864c1c6d134ee73b6
krishkarmyal07 <krishkarmyal96@gmail.com>
2024-08-30 03:03:23 +0530

    style: add content and style welcome comp

--- src/app/main/welcome/welcome.component.ts
+++ src/app/main/welcome/welcome.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class WelcomeComponent {
 
+  constructor(){
+    this.attachMouseMoveListener();
+  }
+
+  attachMouseMoveListener(): void {
+    const body = document.querySelector("body");
+    if (body) {
+      body.addEventListener("mousemove", this.eyeball.bind(this));
+    }
+  }
+
+  eyeball(event: MouseEvent): void {
+    const eyes = document.querySelectorAll<HTMLElement>(".eye");
+
+    eyes.forEach((eye) => {
+      const eyeRect = eye.getBoundingClientRect();
+      const eyeCenterX = eyeRect.left + (eye.clientWidth / 2);
+      const eyeCenterY = eyeRect.top + (eye.clientHeight / 2);
+
+      const radian = Math.atan2(event.pageX - eyeCenterX, event.pageY - eyeCenterY);
+      const rotation = (radian * (180 / Math.PI) * -1) + 270;
+
+      eye.style.transform = `rotate(${rotation}deg)`;
+    });
+  }
 }
