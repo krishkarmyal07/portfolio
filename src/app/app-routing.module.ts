7377bbe235963aa4868aadc68a7afc392e7d0cfc
krishkarmyal07 <krishkarmyal96@gmail.com>
2024-08-30 22:35:16 +0530

    chnage class name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabledBlocking'
+    useHash: true
 })],
   exports: [RouterModule]
 })
