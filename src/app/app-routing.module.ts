f21eb6d5f5dced6688c7708d9218811aca8d28da
krishkarmyal07 <krishkarmyal96@gmail.com>
2024-08-30 21:18:40 +0530

    feat: add error component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainModule } from './main/main.module';
+import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
   {
@@ -11,11 +12,16 @@ const routes: Routes = [
   {
     path:'',
     loadChildren: () => import(`./main/main.module`).then((m)=>m.MainModule)
-  }
+  },
+  {
+    path: 'error',
+    component: ErrorComponent
+  },
+  { path: '**', redirectTo: '/error'},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
