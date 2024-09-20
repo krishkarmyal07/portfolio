72f5c832b466e963069d521e86e4e26c4d25b4b9
krishkarmyal07 <krishkarmyal96@gmail.com>
2024-09-20 20:20:38 +0530

    dark theme

--- src/app/main/setting/setting.component.ts
+++ src/app/main/setting/setting.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class SettingComponent {
 
+  toggleTheme() {
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme === 'dark') {
+      document.body.classList.remove('dark-theme');
+      document.body.classList.add('light-theme');
+      localStorage.setItem('theme', 'light');
+    } else {
+      document.body.classList.remove('light-theme');
+      document.body.classList.add('dark-theme');
+      localStorage.setItem('theme', 'dark');
+    }
+  }
 }
